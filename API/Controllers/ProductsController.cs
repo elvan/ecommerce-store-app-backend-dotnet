8b5a81667072e077ee0930dc7c2302b69b888f03
Elvan Hidayat <elvanhd@gmail.com>
2024-01-14 20:28:40 +0700

    Refactor controllers and add exception handling
    - Implement BaseApiController as a base class for all controllers
    - Refactor ProductsController to inherit from BaseApiController
    - Enhance ProductsController to return NotFound for invalid IDs
    - Create BuggyController for testing various response types
    - Add ExceptionMiddleware for global exception handling
    - Register ExceptionMiddleware in the middleware pipeline
    - Update Program.cs to include ExceptionMiddleware usage

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,14 +1,11 @@
 using API.Data;
 using API.Entities;
-
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class ProductsController : ControllerBase
+public class ProductsController : BaseApiController
 {
     private readonly StoreContext _context;
 
@@ -26,6 +23,10 @@ public class ProductsController : ControllerBase
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-        return await _context.Products.FindAsync(id);
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
 using API.Data;
-
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,6 +22,8 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
