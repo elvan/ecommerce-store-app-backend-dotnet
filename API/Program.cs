49d0ef7e7185494b5e4171584a2ef4132c6e7bdf
Elvan Hidayat <elvanhd@gmail.com>
2024-01-14 10:35:40 +0700

    Enable CORS support for local development
    - Add CORS services in Program.cs for API project
    - Configure CORS to allow any header and any method
    - Set specific origin for CORS: 'http://localhost:3000'
    - Ensure CORS configuration is placed before authorization
    - Integrate CORS setup for better local development experience
    - Facilitate frontend-backend interaction on local setups

--- API/Program.cs
+++ API/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +27,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
