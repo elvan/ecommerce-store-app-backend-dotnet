981dd198f90844b3de4c2fbaf52c099568d5af3b
Elvan Hidayat <elvanhd@gmail.com>
2024-01-18 07:39:54 +0700

    Integrate user authentication and authorization in API
    - Add Microsoft.AspNetCore.Authentication.JwtBearer and Identity.EntityFrameworkCore packages
    - Create AccountController with login, register, and currentUser methods
    - Implement BasketController modifications for user and basket management
    - Introduce DTOs: LoginDto, RegisterDto, and UserDto for user-related operations
    - Update DbInitializer to seed user roles and test users into the database
    - Add IdentityAdded migration for user and role management in the database
    - Configure StoreContext to inherit from IdentityDbContext for user management
    - Create User entity extending IdentityUser for custom user properties
    - Implement BasketExtensions for mapping basket data to DTO
    - Update HttpExtensions to append headers for pagination data
    - Add Identity services, authentication, and JWT token generation in Program.cs
    - Create TokenService for generating JWT tokens for user authentication
    - Update appsettings with JWTSettings for token key configuration
    - Enhance Swagger configuration to include JWT authentication support

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,15 @@
-using API.Data;
-using API.Middleware;
+using System.Text;
 
+using API.Data;
+using API.Entities;
+using API.Middleware;
+using API.Services;
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +19,33 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    // Include 'SecurityScheme' to use JWT Authentication
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put Bearer + your token in the box below",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+});
 
 builder.Services.AddDbContext<StoreContext>(opt =>
 {
@@ -19,6 +53,30 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddCors();
+
+builder.Services.AddIdentityCore<User>(opt =>
+{
+    opt.User.RequireUniqueEmail = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<StoreContext>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -28,7 +86,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization", "true");
+    });
 }
 
 app.UseCors(opt =>
@@ -36,18 +97,20 @@ app.UseCors(opt =>
     opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
 });
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
 
 try
 {
     context.Database.Migrate();
-    DbInitializer.Initialize(context);
+    await DbInitializer.Initialize(context, userManager);
 }
 catch (Exception ex)
 {
